70d6ff57593a98eddbdcff0aeac2d49f71222b50
Kraxpord <114495363+Kraxpord@users.noreply.github.com>
2023-01-21 00:56:39 +0200

    Create ex2.cpp

---
+++ ex2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main()
+{
+	int length;
+	std::cout << "Length in sm";
+	std::cin >> length;
+	float width;
+	std::cout << "Width in sm";
+	std::cin >> width;
+	float height;
+	std::cout << "Height in sm";
+	std::cin >> height;
+	double a;
+	a = length * width* height;
+	std::cout << a << " cubitr";
+
+
+
+	return 0;
+}
