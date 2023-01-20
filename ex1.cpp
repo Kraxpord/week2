9b8d250dd3ab641441ec6c0cf2d5f381e72795a0
Kraxpord <114495363+Kraxpord@users.noreply.github.com>
2023-01-21 00:56:11 +0200

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main()
+{
+	int grn;
+	std::cout << "grn - ";
+	std::cin >> grn;
+	int cop;
+	std::cout << "cop - ";
+	std::cin >> cop;
+	double a;
+	a = cop % 100;
+	double b;
+	b = (cop - a) / 100;
+	grn = grn + b;
+	std::cout << grn << " grn " << a << " cop";
+
+
+
+	return 0;
+}
